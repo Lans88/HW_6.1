71c5664935f792277085ab26357ad7ef575d0c3a
Artem <art001.163@gmail.com>
2022-12-27 22:54:45 +0400

    Исправление замечаний по дз

--- src/main/java/ru/netology/hw61/StatisticService.java
+++ src/main/java/ru/netology/hw61/StatisticService.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 
 public class StatisticService {
     public int sumSale(int[] sales) {
-        int sum = Arrays.stream(sales).sum();
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+
+        }
         return sum;
     }
 
@@ -27,44 +31,44 @@ public class StatisticService {
         return maxMonth + 1;
     }
 
-        public int minSales ( int[] sales){
-            int minMonth = 0;
-            int month = 0; // переменная для индекса рассматриваемого месяца в массиве
-            for (int sale : sales) {
-                // sales[minMonth] - продажи в месяце minMonth
-                // sale - продажи в рассматриваемом месяце
-                if (sale <= sales[minMonth]) {
-                    minMonth = month;
-                }
-                month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+    public int minSales(int[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (int sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
             }
-            return minMonth + 1;
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
         }
-
-        public int amountMonthMinMiddleSale (int[] sales){
-            int middleSumInMonth = Arrays.stream(sales).sum() / 12;
-            int amountMonth = 0;
-            int month = 0;
-            for (int sale : sales) {
-                if (sale < middleSumInMonth) {
-                    amountMonth = amountMonth + 1;
-                }
-
-            }
-            return amountMonth;
-        }
-
-        public int amountMonthMaxMiddleSale ( int[] sales){
-            int middleSumInMonth = Arrays.stream(sales).sum() / 12;
-            int amountMonth = 0;
-            int month = 0;
-            for (int sale : sales) {
-                if (sale > middleSumInMonth) {
-                    amountMonth = amountMonth + 1;
-                }
-
-            }
-            return amountMonth;
-        }
+        return minMonth + 1;
     }
+
+    public int amountMonthMinMiddleSale(int[] sales) {
+        int middleSumInMonth = Arrays.stream(sales).sum() / 12;
+        int amountMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale < middleSumInMonth) {
+                amountMonth = amountMonth + 1;
+            }
+
+        }
+        return amountMonth;
+    }
+
+    public int amountMonthMaxMiddleSale(int[] sales) {
+        int middleSumInMonth = Arrays.stream(sales).sum() / 12;
+        int amountMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+            if (sale > middleSumInMonth) {
+                amountMonth = amountMonth + 1;
+            }
+
+        }
+        return amountMonth;
+    }
+}
 
